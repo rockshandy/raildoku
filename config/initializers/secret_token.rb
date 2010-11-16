19c96b4f79720130b8782893cdc85ceef06ec697
Phil Eberhardt <peberhardt@gmail.com>
2010-11-16 14:57:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raildoku::Application.config.secret_token = '5e01859bfacd588f523fb33987841ae19244157195ce08ee4306151d091198d0cfd6a585a7b9265c03d519e75bacf8d47c94a90ced491bf88562f4ac80ccd344'
