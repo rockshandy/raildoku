64dda5564e8bb369745a3fe902fa4e5a66dc7e3e
Phil Eberhardt <peberhardt@gmail.com>
2010-12-05 18:45:25 -0500

    some integration of constraint and local search.rb files

--- app/controllers/sudoku_controller.rb
+++ app/controllers/sudoku_controller.rb
@@ -1,5 +1,16 @@
+require 'local_search'
 class SudokuController < ApplicationController
+  include LocalSearch
   def index
+    #example
+  end
+
+  # stat a new board, should be ajax
+  def init
+  end
+
+  # should be ajax call eventually to the background
+  def solve
   end
 end
 
